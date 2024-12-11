52c0a9219d4601d78cc0f3b922a365980f723240
SyntaxJason <82037042+SyntaxJason@users.noreply.github.com>
2024-12-11 16:38:26 +0100

    updated some other stuff

--- smoothcloud-node/src/main/java/eu/smoothcloud/node/configuration/LaunchConfiguration.java
+++ smoothcloud-node/src/main/java/eu/smoothcloud/node/configuration/LaunchConfiguration.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class LaunchConfiguration implements JsonSerializable {
+public abstract class LaunchConfiguration implements JsonSerializable {
+
     private String language;
     private String host;
     private int port;
     private int memory;
-
-    public LaunchConfiguration() {}
+    private int threads;
 }
