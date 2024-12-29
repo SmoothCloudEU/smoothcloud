5154c463b4fd04a98b7077282e8f7eb20cba90d8
ezTxmMC <eztxmmc@hotmail.com>
2024-12-30 00:09:59 +0100

    changed dependency enum names and added lombok

--- smoothcloud-launcher/src/main/java/eu/smoothcloud/launcher/dependency/Dependency.java
+++ smoothcloud-launcher/src/main/java/eu/smoothcloud/launcher/dependency/Dependency.java
@@ -1,11 +1,13 @@
 package eu.smoothcloud.launcher.dependency;
 
 public enum Dependency {
-    NETTY("io.netty", "netty5-all", "5.0.0.Alpha5", Repository.MAVEN),
+    NETTY5_ALL("io.netty", "netty5-all", "5.0.0.Alpha5", Repository.MAVEN),
+    LOMBOK("org.projectlombok", "lombok", "1.18.36", Repository.MAVEN),
     JLINE("org.jline", "jline", "3.28.0", Repository.MAVEN),
     TOMLJ("org.tomlj", "tomlj", "1.1.1", Repository.MAVEN),
     JSON("org.json", "json", "20240303", Repository.MAVEN),
-    ANTLR("org.antlr", "antlr4-runtime", "4.11.1", Repository.MAVEN),
+    ANTLR4_RUNTIME("org.antlr", "antlr4-runtime", "4.11.1", Repository.MAVEN),
+    JAVAPOET("com.squareup", "javapoet", "1.13.0", Repository.MAVEN),
     SMOOTHCLOUD_NODE("eu.smoothcloud", "smoothcloud-node", "1.0.0-dev", Repository.SMOOTHCLOUD_CDN);
 
     private final String groupId;
