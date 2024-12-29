d005555d280bf70842585dd7abdfc61cc2a5b92a
ezTxmMC <eztxmmc@hotmail.com>
2024-12-29 21:49:50 +0100

    added init of classloader and dependencyloader

--- smoothcloud-launcher/src/main/java/eu/smoothcloud/launcher/SmoothCloudLauncher.java
+++ smoothcloud-launcher/src/main/java/eu/smoothcloud/launcher/SmoothCloudLauncher.java
@@ -1,17 +1,23 @@
 package eu.smoothcloud.launcher;
 
 import eu.smoothcloud.launcher.dependency.DependencyLoader;
-import eu.smoothcloud.launcher.dependency.InternalDependencyLoader;
 import eu.smoothcloud.launcher.util.JarLoader;
 
 import java.nio.file.Path;
 
 public class SmoothCloudLauncher {
+    private static SmoothCloudClassLoader classLoader;
+    private static DependencyLoader dependencyLoader;
 
     public static void main(String[] args) {
-        DependencyLoader.loadExternalDependencys();
-        InternalDependencyLoader.loadInternalDependencys();
+        classLoader = new SmoothCloudClassLoader();
+        dependencyLoader = new DependencyLoader("dependencies");
+        dependencyLoader.loadDependencys();
         System.out.println("Start Node Module!");
         JarLoader.loadJar(Path.of("dependencies/eu.smoothcloud/smoothcloud-node-1.0.0-dev.jar"), args);
+    }
+
+    public static SmoothCloudClassLoader getClassLoader() {
+        return classLoader;
     }
 }
