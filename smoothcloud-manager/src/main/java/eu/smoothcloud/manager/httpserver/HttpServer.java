b66d83031d8e5725ec6e6e47670cd6f25628bb2c
ezTxmMC <eztxmmc@hotmail.com>
2025-02-06 21:29:06 +0100

    changed rest api dependency and removed paths

--- smoothcloud-manager/src/main/java/eu/smoothcloud/manager/httpserver/HttpServer.java
+++ smoothcloud-manager/src/main/java/eu/smoothcloud/manager/httpserver/HttpServer.java
@@ -15,23 +15,9 @@
 
 package eu.smoothcloud.manager.httpserver;
 
-import eu.smoothcloud.manager.httpserver.api.DocumentationServlet;
-import eu.smoothcloud.manager.httpserver.api.get.player.ListServlet;
-import eu.smoothcloud.manager.httpserver.api.get.player.PlayerServlet;
-import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
-import org.eclipse.jetty.server.Server;
-
 public class HttpServer {
 
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        handler.setContextPath("/");
-        handler.addServlet(DocumentationServlet.class, "/api");
-        handler.addServlet(ListServlet.class, "/api/get/player/list");
-        handler.addServlet(PlayerServlet.class, "/api/get/player/*");
-        server.setHandler(handler);
-        server.start();
-        server.join();
+
     }
 }
